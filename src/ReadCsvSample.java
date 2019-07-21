e4a18d41c5013b6c24ef2740b1dc8554f4469631
itohsunn <tyuke.net@gmail.com>
2019-07-22 00:20:27 +0900

    csv を読み込み１行ずつリスト化する（未完成）

--- src/ReadCsvSample.java
+++ src/ReadCsvSample.java
@@ -10,11 +10,19 @@ public class ReadCsvSample {
 
         String csvFile = "text.csv";
         String filePath = "";
+        String[] list = null;
 
         filePath = getCsvFile(csvFile);
         if (filePath != ""){
-            readFileStream(filePath);
+//            readFileStream(filePath);
+            list = reacCsvLines(filePath);
         }
+
+        for (String line : list) {
+            System.out.println(line);
+        }
+
+
     }
 
     private static String getCsvFile(String csvFile) {
@@ -56,4 +64,43 @@ public class ReadCsvSample {
 
         }
     }
+
+
+    private static String[] reacCsvLines(String filePath) {
+
+        String[] list = null;
+        int lineCount = 0;
+        int i = 0;
+        try {
+
+            FileInputStream in = null;
+            InputStreamReader is = null;
+            BufferedReader br = null;
+
+            in = new FileInputStream(filePath);
+            is = new InputStreamReader(in);
+            br = new BufferedReader(is);
+
+            String line = "";
+
+            // 1行ずつ読み込み
+            while ((line = br.readLine()) !=  null) {
+                lineCount++;
+            }
+
+            list = new String[lineCount];
+
+            while ((line = br.readLine()) !=  null) {
+                list[i] = String.join(line);
+                i++;
+            }
+
+        } catch (IOException e) {
+            System.out.println(e.toString());
+
+        }
+        return list;
+    }
+
+
 }
